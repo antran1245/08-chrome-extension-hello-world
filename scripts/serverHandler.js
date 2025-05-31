93cb6b0ea57310308edef448aa747eafdc18ca5c
An <antran1245@gmail.com>
2025-05-31 16:04:49 -0700

    tested api called to server with data

--- scripts/serverHandler.js
+++ scripts/serverHandler.js
@@ -24,3 +24,53 @@ apiCallButton.addEventListener("click", () => {
     .then((data) => console.log(data))
     .catch((error) => console.error("Error:", error));
 });
+
+/**
+ * Save current tab to database by API called.
+ */
+const saveTabButton = document.querySelector("#saveTabButton");
+saveTabButton.addEventListener("click", () => {
+  const title =
+    currentTab["title"] != "" ? currentTab["title"] : currentTab["url"];
+  const url = currentTab["url"];
+  const description =
+    currentTab["title"] != "" ? currentTab["title"] : currentTab["url"];
+  const data = { title, url, description };
+  fetch("http://127.0.0.1:5000/url/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error in adding URL:", error));
+});
+/**
+ * {
+    "active": true,
+    "audible": true,
+    "autoDiscardable": true,
+    "discarded": false,
+    "favIconUrl": "https://www.youtube.com/s/desktop/428d9196/img/logos/favicon_32x32.png",
+    "frozen": false,
+    "groupId": -1,
+    "height": 911,
+    "highlighted": true,
+    "id": 2078446582,
+    "incognito": false,
+    "index": 5,
+    "lastAccessed": 1748721631268.859,
+    "mutedInfo": {
+        "muted": false
+    },
+    "pinned": false,
+    "selected": true,
+    "status": "complete",
+    "title": "TL vs DIG - PLAYOFFS 2025 LTA North Split 2 - W9D1 - YouTube",
+    "url": "https://www.youtube.com/watch?v=GyliB_kDb0I&ab_channel=LTANorth",
+    "width": 1920,
+    "windowId": 2078446493
+}
+ */
--- scripts/tab.js
+++ scripts/tab.js
@@ -1,6 +1,11 @@
 // Options for tabs query
 let tabQueryOptions = {};
 
+// Tabs Array
+let tabsArray = [];
+// Current Tab
+let currentTab = {};
+
 /**
  * Retrieves an array of object with tabs and details.
  *
@@ -29,9 +34,50 @@ const tabButton = document.querySelector("#getTabsButton");
  */
 tabButton.addEventListener("click", async () => {
   try {
-    let tabs = await getTabs();
-    console.log(tabs);
+    tabsArray = await getTabs();
+    console.log(tabsArray);
   } catch (error) {
     console.log("Error fetching tabs: ", error);
   }
 });
+
+/**
+ * Print the tabs array
+ */
+const printTabsButton = document.querySelector("#printTabsArray");
+printTabsButton.addEventListener("click", () => {
+  console.log(tabsArray);
+});
+
+/**
+ * Return the active tab the extension is running.
+ * @returns {Promise<Object>} Tab Object
+ */
+function getCurrentTab() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(tab[0]);
+      }
+    });
+  });
+}
+
+/**
+ * Connect a button test to run getCurrentTab.
+ */
+const currentTabButton = document.querySelector("#getCurrentTab");
+currentTabButton.addEventListener("click", async () => {
+  let test = await getCurrentTab();
+});
+
+/**
+ * Get the current tabs on startup of extension
+ */
+async function startup() {
+  tabsArray = await getTabs();
+  currentTab = await getCurrentTab();
+}
+startup();
